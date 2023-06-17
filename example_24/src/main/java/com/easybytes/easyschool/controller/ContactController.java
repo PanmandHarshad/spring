a74fcc53e899e5b066fc41e3fb19b97e37c9d056
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-06-17 21:02:49 +0530

    Lecture 98: Added validation annotation in Contact record Lecture 99: Adding bean validation changes in contact.html and ContactController

--- example_24/src/main/java/com/easybytes/easyschool/controller/ContactController.java
+++ example_24/src/main/java/com/easybytes/easyschool/controller/ContactController.java
@@ -2,15 +2,19 @@ package com.easybytes.easyschool.controller;
 
 import com.easybytes.easyschool.model.Contact;
 import com.easybytes.easyschool.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -24,7 +28,8 @@ public class ContactController {
     }
 
     @RequestMapping(value = {"/contact"})
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact("", "", "", "", ""));
         return "contact.html";
     }
 
@@ -43,9 +48,15 @@ public class ContactController {
 //    }
 
     @RequestMapping(value = "saveMsg", method = RequestMethod.POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation is failed due to: " + errors);
+            return "contact.html";
+        }
+
         contactService.saveMessageDetails(contact);
 
-        return new ModelAndView("redirect:/contact");
+        // This "redirect:/contact" is loading the fresh page
+        return "redirect:/contact";
     }
 }

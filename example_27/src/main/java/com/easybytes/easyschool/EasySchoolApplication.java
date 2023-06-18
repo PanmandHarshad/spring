f9b240d33011590df197c477d9a2f7dd063b0db2
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-06-18 13:32:33 +0530

    Example 27

---
+++ example_27/src/main/java/com/easybytes/easyschool/EasySchoolApplication.java
@@ -0,0 +1,14 @@
+package com.easybytes.easyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EasySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySchoolApplication.class, args);
+	}
+
+}

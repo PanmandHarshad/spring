daf833981599fd88cfb9e75f801befd35d0bba7b
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-11 19:36:32 +0530

    Lecture 176 : Updating person data in database

--- example_40/src/main/java/com/easybytes/easyschool/model/Profile.java
+++ example_40/src/main/java/com/easybytes/easyschool/model/Profile.java
@@ -32,11 +32,11 @@ public class Profile {
     private String address2;
 
     @NotBlank(message = "City must not be blank")
-    @Size(min = 5, message = "City must be at least 5 characters long")
+    @Size(min = 4, message = "City must be at least 4 characters long")
     private String city;
 
     @NotBlank(message = "State must not be blank")
-    @Size(min = 5, message = "State must be at least 5 characters long")
+    @Size(min = 3, message = "State must be at least 3 characters long")
     private String state;
 
     @NotBlank(message = "Zip Code must not be blank")

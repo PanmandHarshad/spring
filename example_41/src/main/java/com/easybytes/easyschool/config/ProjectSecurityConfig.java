e65f695597710fd36b807c5b1d794241bf423eed
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-11 20:28:24 +0530

    Lecture 178: Created class table, added admin controller, added classes.html

--- example_41/src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
+++ example_41/src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
@@ -26,6 +26,7 @@ public class ProjectSecurityConfig {
                 requests
                         .requestMatchers("/dashboard").authenticated()
                         .requestMatchers("/displayMessages").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         // Added '/**' to '/closeMsg' because we are passing Id as query param from
                         // It is better to have '/**' because if in future if we change query param
                         // then we don't have to change here

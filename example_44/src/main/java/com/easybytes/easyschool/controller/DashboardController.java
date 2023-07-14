5a6fea151227a203912c640ad122ba50d43dfc34
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-14 19:34:09 +0530

    Example 44

---
+++ example_44/src/main/java/com/easybytes/easyschool/controller/DashboardController.java
@@ -0,0 +1,34 @@
+package com.easybytes.easyschool.controller;
+
+import com.easybytes.easyschool.model.Person;
+import com.easybytes.easyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @RequestMapping("/dashboard")
+    public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession) {
+        Person person = personRepository.readByEmail(authentication.getName());
+
+        model.addAttribute("username", person.getName());
+        model.addAttribute("roles", authentication.getAuthorities().toString());
+        httpSession.setAttribute("loggedInPerson", person);
+
+        if (null != person.getEazyClass() && null != person.getEazyClass().getName()) {
+            model.addAttribute("enrolledClass", person.getEazyClass().getName());
+        }
+
+        //throw new RuntimeException("It's been a bad day !!");
+        return "dashboard.html";
+    }
+
+}

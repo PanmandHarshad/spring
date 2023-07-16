7b311e427d0fa87ea0bb180e31aeffc63f65e7b0
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-16 19:48:36 +0530

    Lecture 210: Close message operation

--- example_45/src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
+++ example_45/src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
@@ -1,5 +1,6 @@
 package com.easybytes.easyschool.rest;
 
+import com.easybytes.easyschool.constants.EazySchoolConstants;
 import com.easybytes.easyschool.model.Contact;
 import com.easybytes.easyschool.model.Response;
 import com.easybytes.easyschool.repository.ContactRepository;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -63,4 +65,27 @@ public class ContactRestController {
                 .status(HttpStatus.OK)
                 .body(response);
     }
+
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact contactReq) {
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(contactReq.getContactId());
+
+        if (contact.isPresent()) {
+            contact.get().setStatus(EazySchoolConstants.CLOSE);
+            contactRepository.save(contact.get());
+        } else {
+            response.setStatusCode("400");
+            response.setStatusMsg("Invalid contact ID received");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully closed");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
 }

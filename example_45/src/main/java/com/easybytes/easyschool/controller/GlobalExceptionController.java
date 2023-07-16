a3abde4705af231f62b023aeab1effa80aabdefd
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-16 20:09:17 +0530

    Lecture 212: Global error logic for REST services using @RestControllerAdvice

--- example_45/src/main/java/com/easybytes/easyschool/controller/GlobalExceptionController.java
+++ example_45/src/main/java/com/easybytes/easyschool/controller/GlobalExceptionController.java
@@ -1,6 +1,7 @@
 package com.easybytes.easyschool.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class GlobalExceptionController {
 
     /**

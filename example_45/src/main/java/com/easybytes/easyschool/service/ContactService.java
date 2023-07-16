6507ec1746739be508bfd765efd42b8ea5400da9
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-16 18:01:06 +0530

    Example 45

---
+++ example_45/src/main/java/com/easybytes/easyschool/service/ContactService.java
@@ -0,0 +1,75 @@
+package com.easybytes.easyschool.service;
+
+import com.easybytes.easyschool.constants.EazySchoolConstants;
+import com.easybytes.easyschool.model.Contact;
+import com.easybytes.easyschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class ContactService {
+
+    // This will invoke the ContactRepository from service layer
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = false;
+
+        contact.setStatus(EazySchoolConstants.OPEN);
+        Contact savedContact = contactRepository.save(contact);
+
+        if (savedContact.getContactId() > 0) {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+
+    public List<Contact> findMsgsWithOpenStatus() {
+        return contactRepository.findByStatus(EazySchoolConstants.OPEN);
+    }
+
+    public Page<Contact> findMsgsWithOpenStatus(int pageNum, String sortField, String sortDir) {
+        int pageSize = 5;
+
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
+                sortDir.equals("asc") ? Sort.by(sortField).ascending() :
+                        Sort.by(sortField).descending());
+
+        return contactRepository.findByStatus(EazySchoolConstants.OPEN, pageable);
+        // return contactRepository.findOpenMsgs(EazySchoolConstants.OPEN, pageable);
+        // Named Native Query method does not support dynamic sorting
+        // return contactRepository.findOpenMsgsNative(EazySchoolConstants.OPEN, pageable);
+    }
+
+    public boolean updateMsgStatus(int contactId) {
+        boolean isUpdated = false;
+
+        /*
+        Optional<Contact> contact = contactRepository.findById(contactId);
+        contact.ifPresent(contact1 -> contact1.setStatus(EazySchoolConstants.CLOSE));
+
+        Contact updatedContact = contactRepository.save(contact.get());
+        if (updatedContact.getUpdatedBy() != null) {
+            isUpdated = true;
+        }*/
+
+        //int rowsModified = contactRepository.updateStatusById(EazySchoolConstants.CLOSE, contactId);
+        //int rowsModified = contactRepository.updateMsgStatus(EazySchoolConstants.CLOSE, contactId);
+        int rowsModified = contactRepository.updateMsgStatusNative(EazySchoolConstants.CLOSE, contactId);
+        if (rowsModified > 0) {
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
+}

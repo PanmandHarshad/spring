a315a57a4cd0ed77e36850c5971425cf1d104de1
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-19 16:14:37 +0530

    Lecture 226: Giving path name to Rest resource and hiding repositories from exposing.

--- example_47/src/main/java/com/easybytes/easyschool/repository/CoursesRepository.java
+++ example_47/src/main/java/com/easybytes/easyschool/repository/CoursesRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+//@RepositoryRestResource(path = "courses")
+//@RepositoryRestResource(exported = false)
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
 
     /* *

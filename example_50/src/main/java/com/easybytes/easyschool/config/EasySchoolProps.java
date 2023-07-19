d915b551f6e9c6804247886b2178445b66210815
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-19 20:10:51 +0530

    Example 50

---
+++ example_50/src/main/java/com/easybytes/easyschool/config/EasySchoolProps.java
@@ -0,0 +1,24 @@
+package com.easybytes.easyschool.config;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Map;
+
+@Component("easySchoolProps")
+@Data
+@ConfigurationProperties(prefix = "easyschool")
+@Validated
+public class EasySchoolProps {
+
+    @Min(value = 5, message = "Must be between 5 and 25")
+    @Max(value = 25, message = "Must be between 5 and 25")
+    private int pageSize;
+    private Map<String, String> contact;
+    private List<String> branches;
+}

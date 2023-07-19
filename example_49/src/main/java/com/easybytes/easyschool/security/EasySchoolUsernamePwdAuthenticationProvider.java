07a26392a905936b4dde889b314e715057f9fc0f
Harshad Panmand <harshad.panmand@kristasoft.com>
2023-07-19 17:51:26 +0530

    Example 49

---
+++ example_49/src/main/java/com/easybytes/easyschool/security/EasySchoolUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,56 @@
+package com.easybytes.easyschool.security;
+
+import com.easybytes.easyschool.model.Person;
+import com.easybytes.easyschool.model.Roles;
+import com.easybytes.easyschool.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EasySchoolUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        Person person = personRepository.readByEmail(email);
+
+        if (null != person && person.getPersonId() > 0 && passwordEncoder.matches(password, person.getPwd())) {
+            return new UsernamePasswordAuthenticationToken(
+                    email, // This will be considered as authenticated name
+                    null,
+                    getGrantedAuthorities(person.getRoles()));
+        } else {
+            throw new BadCredentialsException("Invalid credentials!");
+        }
+    }
+
+    // This method first get called and if it returns true then only authenticate() method will get called
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Roles roles) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + roles.getRoleName()));
+        return grantedAuthorities;
+    }
+}
---
+++ example_49/src/main/java/com/easybytes/easyschool/service/PersonService.java
@@ -0,0 +1,34 @@
+package com.easybytes.easyschool.service;
+
+
+import com.easybytes.easyschool.constants.EazySchoolConstants;
+import com.easybytes.easyschool.model.Person;
+import com.easybytes.easyschool.model.Roles;
+import com.easybytes.easyschool.repository.PersonRepository;
+import com.easybytes.easyschool.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    private RolesRepository rolesRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean createNewPerson(Person person) {
+        boolean isSaved = false;
+        Roles role = rolesRepository.getByRoleName(EazySchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
+        person = personRepository.save(person);
+        if (person.getPersonId() > 0) {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+}
